48e4fe37485d45642d6fd9c2ecf6edf977ea0984
geoffrey jost <geoffrey@jostwebwerks.com>
2013-02-12 17:35:14 -0800

    entity, control file, and mets objects
    Not fully-modelled objects; rather, just enough to make it easy
    to read the metadata files and do file checksumming on the entity.

--- kura/Kura/collection.py
+++ kura/Kura/collection.py
@@ -1,8 +1,11 @@
+import ConfigParser
 from datetime import datetime
+import hashlib
 import os
 import subprocess
 import sys
 
+from bs4 import BeautifulSoup
 import git
 
 
@@ -51,6 +54,190 @@ def write_changelog_entry(path, messages, user, email, debug=False):
         if preexisting:
             changelog.write('\n')
         changelog.write(entry)
+
+
+class ControlFile( object ):
+    """control file inspired by Debian package control file but using INI syntax.
+    """
+    path = None
+    _config = None
+    CHECKSUMS = ['sha1', 'sha256', 'files']
+    
+    def __init__( self, path, debug=False ):
+        self.path = path
+        if not os.path.exists(self.path):
+            print('ERR: control file not initialized')
+            sys.exit(1)
+        self.read(debug=debug)
+    
+    def read( self, debug=False ):
+        if debug:
+            print('Reading control file {} ...'.format(self.path))
+        self._config = ConfigParser.ConfigParser()
+        self._config.read([self.path])
+    
+    def write( self, debug=False ):
+        if debug:
+            print('Writing control file {} ...'.format(self.path))
+        with open(self.path, 'w') as cfile:
+            self._config.write(cfile)
+    
+    def entity_update_checksums( self, entity, debug=False ):
+        # return relative path to payload
+        def relative_path(entity_path, payload_file):
+            if entity_path[-1] != '/':
+                entity_path = '{}/'.format(entity_path)
+            return payload_file.replace(entity_path, '')
+        
+        self._config.remove_section('Checksums-SHA1')
+        self._config.add_section('Checksums-SHA1')
+        for sha1,path in entity.checksums('sha1', debug=debug):
+            path = relative_path(entity.path, path)
+            self._config.set('Checksums-SHA1', sha1, path)
+        #
+        self._config.remove_section('Checksums-SHA256')
+        self._config.add_section('Checksums-SHA256')
+        for sha256,path in entity.checksums('sha256', debug=debug):
+            path = relative_path(entity.path, path)
+            self._config.set('Checksums-SHA256', sha256, path)
+        #
+        self._config.remove_section('Files')
+        self._config.add_section('Files')
+        for md5,path in entity.checksums('md5', debug=debug):
+            size = os.path.getsize(path)
+            path = relative_path(entity.path, path)
+            self._config.set('Files', md5, '{} ; {}'.format(size,path))
+
+
+class METS( object ):
+    """Metadata Encoding and Transmission Standard (METS) file.
+    """
+    entity_path = None
+    filename = None
+    soup = None
+    
+    def __init__( self, entity, debug=False ):
+        self.entity_path = entity.path
+        self.filename = os.path.join(self.entity_path, 'mets.xml')
+        self.read(debug=debug)
+        if debug:
+            print(self.soup.prettify())
+    
+    def read( self, debug=False ):
+        if debug:
+            print('Reading METS file {}'.format(self.filename))
+        with open(self.filename, 'r') as mfile:
+            self.soup = BeautifulSoup(mfile, 'xml')
+    
+    def write( self, debug=False ):
+        if debug:
+            print('Writing METS file {}'.format(self.filename))
+        with open(self.filename, 'w') as mfile:
+            mfile.write(self.soup.prettify())
+    
+    def update_filesec( self, entity, debug=False ):
+        """
+        <fileSec>
+          <fileGrp USE="master">
+            <file GROUPID="GID1" ID="FID1" ADMID="AMD1" SEQ="1" MIMETYPE="image/tiff" CHECKSUM="80172D87C6A762C0053CAD9215AE2535" CHECKSUMTYPE="MD5">
+              <FLocat LOCTYPE="OTHER" OTHERLOCTYPE="fileid" xlink:href="1147733144860875.tiff"/>
+            </file>
+          </fileGrp>
+          <fileGrp USE="usecopy">
+            <file GROUPID="GID1" ID="FID2" ADMID="AMD2" SEQ="1" MIMETYPE="image/jpeg" CHECKSUM="4B02150574E1B321B526B095F82BBA0E" CHECKSUMTYPE="MD5">
+              <FLocat LOCTYPE="OTHER" OTHERLOCTYPE="fileid" xlink:href="1147733144860875.jpg"/>
+            </file>
+          </fileGrp>
+        </fileSec>
+        """
+        payload_path = entity.payload_path()
+        
+        # return relative path to payload
+        def relative_path(entity_path, payload_file):
+            if entity_path[-1] != '/':
+                entity_path = '{}/'.format(entity_path)
+            return payload_file.replace(entity_path, '')
+        n = 0
+        # remove existing files
+        filesec = self.soup.new_tag('fileSec')
+        self.soup.fileSec.replace_with(filesec)
+        # add new ones
+        for md5,path in entity.checksums('md5', debug=debug):
+            n = n + 1
+            use = 'unknown'
+            seq = n
+            gid = 'GID{}'.format(n)
+            fid = 'FID{}'.format(n)
+            aid = 'AMD{}'.format(n)
+            mimetype = 'mimetype'
+            path = relative_path(entity.path, path)
+            # add fileGrp, file, Floca
+            fileGrp = self.soup.new_tag('fileGrp', USE='master')
+            self.soup.fileSec.append(fileGrp)
+            f = self.soup.new_tag('file',
+                                  GROUPID=gid, ID=fid, ADMID=aid, SEQ=seq, MIMETYPE=mimetype,
+                                  CHECKSUM=md5, CHECKSUMTYPE='md5')
+            fileGrp.append(f)
+            flocat = self.soup.new_tag('Flocat',
+                                       LOCTYPE='OTHER', OTHERLOCTYPE='fileid',
+                                       href=path)
+            f.append(flocat)
+
+
+class Entity( object ):
+    path = None
+    
+    def __init__( self, path, uid=None, debug=False ):
+        self.path = path
+        if not uid:
+            uid = os.path.basename(self.path)
+        self.uid  = uid
+    
+    def payload_path( self, trailing_slash=False ):
+        p = os.path.join(self.path, 'files')
+        if trailing_slash:
+            p = '{}/'.format(p)
+        return p
+    
+    def files( self ):
+        """Returns relative paths to payload files."""
+        files = []
+        entity_path = self.path
+        if entity_path[-1] != '/':
+            entity_path = '{}/'.format(entity_path)
+        for f in os.listdir(self.payload_path()):
+            files.append(f.replace(entity_path, ''))
+        return files
+
+    @staticmethod
+    def checksum_algorithms():
+        return ['md5', 'sha1', 'sha256']
+    
+    def checksums( self, algo, debug=False ):
+        checksums = []
+        def file_checksum( path, algo, block_size=1024 ):
+            if algo == 'md5':
+                h = hashlib.md5()
+            elif algo == 'sha1':
+                h = hashlib.sha1()
+            else:
+                return None
+            f = open(path, 'rb')
+            while True:
+                data = f.read(block_size)
+                if not data:
+                    break
+                h.update(data)
+            f.close()
+            return h.hexdigest()
+        if algo not in Entity.checksum_algorithms():
+            raise Error('BAD ALGORITHM CHOICE: {}'.format(algo))
+        for f in self.files():
+            fpath = os.path.join(self.payload_path(), f)
+            cs = file_checksum(fpath, algo)
+            if cs:
+                checksums.append( (cs, fpath) )
+        return checksums
 
 
 
