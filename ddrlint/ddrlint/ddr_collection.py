01147e4f8ffde7dbdfdca6e0f549900d83df56a5
geoffrey jost <geoffrey.jost@densho.org>
2013-05-31 14:04:55 -0700

    replace unittest framework with roll-yer-own TAP
    unittest is great for testing code but isn't flexible enough for
    this project.  In particular I needed to be able to return lists
    of files/entities/things that don't pass tests, and the output
    needs to be parsable by other programs.
    TAP (Test Anything Protocol) seems like a good candidate.
    The new code should make it easier to return a list of tests and
    results when called as a library, and nicely formatted text when
    run from the command line.

--- ddrlint/ddrlint/ddr_collection.py
+++ ddrlint/ddrlint/ddr_collection.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import hashlib
+import json
 import os
 import re
 import StringIO
-import unittest
 
 from dateutil.parser import parse
 from lxml import etree
+import yaml
 
 
 """
@@ -26,7 +27,10 @@ def _parse_xml(path):
     """Indicate whether file can be parsed as XML.
     @return tree: lxml tree object
     """
-    return etree.parse(StringIO.StringIO(_read_xml(path)))
+    raw = _read_xml(path)
+    if raw:
+        return etree.parse(StringIO.StringIO(raw))
+    return None
 
 def _validate_xml(path):
     """Indicate whether XML file is valid.
@@ -38,7 +42,9 @@ def _entity_eids(ead_path):
     """List of entity EIDs from the EAD.xml.
     """
     tree = _parse_xml(ead_path)
-    return tree.xpath('/ead/dsc/c01/did/unittitle/@eid')
+    if tree:
+        return tree.xpath('/ead/dsc/c01/did/unittitle/@eid')
+    return []
 
 def _entity_paths(files_path, eids):
     """List of entity directory paths.
@@ -88,34 +94,35 @@ def _changelog_valid(path):
     - space after datetime
     """
     valid = False
-    with open(path, 'r') as f:
-        changelog = f.read()
-    num_valid = 0
-    entries = changelog.split(' \n\n')
-    pattern = '^-- ([\w ]+) <([\w_.-@]+)>$'
-    for entry in entries:
-        lines = entry.strip().split('\n')
-        # signature
-        sig = lines.pop().strip()
-        # name, email formatted properly
-        name = None; mail = None
-        name_mail = sig.split('  ')[0]
-        if name_mail:
-            m = re.match(pattern, name_mail)
-            name = m.group(1)
-            mail = m.group(2)
-        # timestamp can be parsed into datetime
-        timestamp = None
-        try:    tsraw = sig.split('  ')[1]
-        except: tsraw = ''
-        try:    ts = parse(tsraw)
-        except: ts = None
-        if ts and (type(ts) == type(datetime(1970,1,1,00,00,00))):
-            timestamp = ts
-        if name and mail and timestamp:
-            num_valid = num_valid + 1
-    if num_valid == len(entries):
-        valid = True
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            changelog = f.read()
+        num_valid = 0
+        entries = changelog.split(' \n\n')
+        pattern = '^-- ([\w ]+) <([\w_.-@]+)>$'
+        for entry in entries:
+            lines = entry.strip().split('\n')
+            # signature
+            sig = lines.pop().strip()
+            # name, email formatted properly
+            name = None; mail = None
+            name_mail = sig.split('  ')[0]
+            if name_mail:
+                m = re.match(pattern, name_mail)
+                name = m.group(1)
+                mail = m.group(2)
+            # timestamp can be parsed into datetime
+            timestamp = None
+            try:    tsraw = sig.split('  ')[1]
+            except: tsraw = ''
+            try:    ts = parse(tsraw)
+            except: ts = None
+            if ts and (type(ts) == type(datetime(1970,1,1,00,00,00))):
+                timestamp = ts
+            if name and mail and timestamp:
+                num_valid = num_valid + 1
+        if num_valid == len(entries):
+            valid = True
     return valid
 
 def _control_valid(path):
@@ -216,199 +223,254 @@ def _entity_files_verified(entity_files_info):
                     verified.append(f)
     return verified
 
+
+
+# TESTS ================================================================
+
+
+TEST_SUITE_VERSION = 'ddr-collection-0.1'
+OK = 'ok'
+FAIL = 'not ok'
+ERR = 'error'
+EMIT_STYLE = 'tap'
+
+def _emit(code, msg, data=None):
+    if EMIT_STYLE == 'tap':
+        tap = ['{} - {}'.format(code, msg)]
+        if data:
+            for line in yaml.dump(data, default_flow_style=False).split('\n'):
+                tap.append('    {}'.format(line))
+        return '\n'.join(tap).strip()
+    elif EMIT_STYLE == 'json':
+        if data:
+            return '{}: {} : {}'.format(code, msg, json.dumps(data))
+        return '{} - {}'.format(code, msg)
+    return 'set emit style'
     
 
 
-class TestDDRCollection(unittest.TestCase):
-    path = ''
-    ead_path = ''
-    changelog_path = ''
-    files_path = ''
+def test00_collection_directory_exists(path):
+    if os.path.exists(path):
+        return _emit(OK, 'collection directory')
+    return _emit(FAIL, 'collection directory not found', {'path':path})
+        
 
-    def test00_collection_directory_exists(self):
-        """Collection directory does not exist.
-        """
-        self.assertEqual(True, os.path.exists(self.path))
-    
-    # ead.xml ----------------------------------------------------------
-    
-    def test010_ead_exists(self):
-        """Collection directory does not contain an ead.xml file.
-        """
-        self.assertEqual(True, os.path.exists(self.ead_path))
-    
-    def test011_ead_readable(self):
-        """Collection ead.xml is not readable.
-        """
-        readable = False
-        raw = _read_xml(self.ead_path)
-        if raw:
-            readable = True
-        self.assertEqual(True, readable)
-    
-    def test012_ead_parsable(self):
-        """Collection ead.xml is not parsable.
-        """
-        parsable = False
-        tree = _parse_xml(self.ead_path)
-        if tree:
-            parsable = True
-        self.assertEqual(True, parsable)
-    
-    def test013_ead_valid(self):
-        """Collection ead.xml is not valid.
-        """
-        valid = False
-        self.assertEqual(True, valid)
-    
-    # changelog file ---------------------------------------------------
-    
-    def test020_changelog_exists(self):
-        """Collection directory does not contain a changelog file.
-        """
-        self.assertEqual(True, os.path.exists(self.changelog_path))
-    
-    def test021_changelog_valid(self):
-        """Collection changelog file is not valid.
-        """
-        valid = _changelog_valid(self.changelog_path)
-        self.assertEqual(True, valid)
+# changelog file ---------------------------------------------------
 
-    
-    # control file -----------------------------------------------------
-    
-    def test030_control_exists(self):
-        """Collection directory does not contain a control file.
-        """
-        self.assertEqual(True, os.path.exists(self.control_path))
-    
-    def test031_control_valid(self):
-        """Collection control file is not valid.
-        """
-        valid = _control_valid(self.control_path)
-        self.assertEqual(True, valid)
-    
-    # entities =========================================================
-    
-    def test040_entities_dir_exists(self):
-        """Collection directory does not contain a files directory.
-        """
-        if self.entity_eids:
-            self.assertEqual(True, os.path.exists(self.files_path))
-            self.assertEqual(True, os.path.isdir(self.files_path))
-    
-    def test041_entity_dirs_exist(self):
-        """Entities listed in manifest do not exist.
-        """
-        self.assertEqual(len(self.entity_eids),
-                         len(self.entity_dirs))
-    
-    # entity changelogs ------------------------------------------------
-    
-    def test0420_entity_changelogs_exist(self):
-        """Entity changelogs are missing.
-        """
-        self.assertEqual(len(self.entity_changelog_paths),
-                         len(self.entity_paths))
-    
-    def test0421_entity_changelogs_valid(self):
-        """Entity changelogs are not valid.
-        """
-        self.assertEqual(True, False)
-    
-    # entity control files ---------------------------------------------
-    
-    def test0430_entity_controls_exist(self):
-        """Entity control files are missing.
-        """
-        self.assertEqual(len(self.entity_control_paths),
-                         len(self.entity_paths))
-    
-    def test0431_entity_controls_valid(self):
-        """Entity control files are not vald.
-        """
-        self.assertEqual(True, False)
-    
-    # entity mets.xml --------------------------------------------------
-    
-    def test0440_entity_mets_exist(self):
-        """Entity mets.xml files are missing.
-        """
-        self.assertEqual(len(self.entity_metsxml_paths),
-                         len(self.entity_paths))
-    
-    def test0441_entity_mets_readable(self):
-        """Entity mets.xml files are not readable.
-        """
-        self.assertEqual(len(self.entity_metsxml_readable),
-                         len(self.entity_paths))
-    
-    def test0442_entity_mets_parsable(self):
-        """Entity mets.xml files are not parsable.
-        """
-        self.assertEqual(len(self.entity_metsxml_parsable),
-                         len(self.entity_paths))
-    
-    def test0443_entity_mets_valid(self):
-        """Entity mets.xml files are not valid.
-        """
-        self.assertEqual(len(self.entity_metsxml_valid),
-                         len(self.entity_paths))
+def test020_changelog_exists(path):
+    if os.path.exists(path):
+        return _emit(OK, 'collection changelog')
+    return _emit(FAIL, 'collection changelog file not found', {'path':path})
+
+def test021_changelog_valid(path):
+    if _changelog_valid(path):
+        return _emit(OK, 'Collection changelog file is valid')
+    return _emit(FAIL, 'Collection changelog file is not valid', {'path':path})
+ 
+# control file -----------------------------------------------------
+
+def test030_control_exists(path):
+    if os.path.exists(path):
+        return _emit(OK, 'collection control file')
+    return _emit(FAIL, 'collection control file not found', {'path':path})
+
+def test031_control_valid(path):
+    if _control_valid(path):
+        return _emit(OK, 'collection control file is valid')
+    return _emit(FAIL, 'collection control file is not valid', {'path':path})
+
+# ead.xml ----------------------------------------------------------
+
+def test010_ead_exists(path):
+    if os.path.exists(path):
+        return _emit(OK, 'ead.xml')
+    return _emit(FAIL, 'ead.xml file not found', {'path':path})
+
+def test011_ead_readable(path):
+    raw = _read_xml(path)
+    if raw:
+        return _emit(OK, 'ead.xml is readable')
+    return _emit(FAIL, 'ead.xml is not readable', {'path':path})
+
+def test012_ead_parsable(path):
+    tree = _parse_xml(path)
+    if tree:
+        return _emit(OK, 'ead.xml is parsable')
+    return _emit(FAIL, 'Collection ead.xml is not parsable', {'path':path})
+
+def test013_ead_valid(path):
+    return _emit(FAIL, 'Collection ead.xml is not valid', {'path':path})
+
+# entities =========================================================
+
+def test040_entities_dir_exists(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        return _emit(OK, 'collection entities dir')
+    return _emit(FAIL, 'collection entities dir not found', {'path':path})
+
+def test041_entity_dirs_exist(entity_eids, entity_paths):
+    passed = []
+    failed = []
+    for path in entity_paths:
+        if os.path.exists(path):
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_eids):
+        return _emit(OK, 'entity dirs')
+    else:
+        return _emit(FAIL, 'entity dirs missing', failed)
+
+# entity changelogs ------------------------------------------------
+
+def test0420_entity_changelogs_exist(entity_changelog_paths, entity_paths):
+    passed = []
+    failed = []
+    for path in entity_changelog_paths:
+        if os.path.exists(path):
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_paths):
+        return _emit(OK, 'entity changelogs')
+    else:
+        return _emit(FAIL, 'entity changelogs missing', failed)
+
+def test0421_entity_changelogs_valid(entity_changelog_paths):
+    return _emit(FAIL, 'entity changelogs not valid')
+
+# entity control files ---------------------------------------------
+
+def test0430_entity_controls_exist(entity_control_paths, entity_paths):
+    passed = []
+    failed = []
+    for path in entity_control_paths:
+        if os.path.exists(path):
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_paths):
+        return _emit(OK, 'entity control files')
+    else:
+        return _emit(FAIL, 'entity control files missing', failed)
+
+def test0431_entity_controls_valid(entity_control_paths):
+    return _emit(FAIL, 'entity control files not valid')
+
+# entity mets.xml --------------------------------------------------
+
+def test0440_entity_metsxml_exist(entity_metsxml_paths, entity_paths):
+    passed = []
+    failed = []
+    for path in entity_metsxml_paths:
+        if os.path.exists(path):
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_paths):
+        return _emit(OK, 'entity mets.xml files')
+    else:
+        return _emit(FAIL, 'entity mets.xml files missing', failed)
+
+def test0441_entity_mets_readable(entity_metsxml_readable, entity_metsxml_paths):
+    passed = []
+    failed = []
+    for path in entity_metsxml_paths:
+        if path in entity_metsxml_readable:
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_metsxml_paths):
+        return _emit(OK, 'entity mets.xml files readable')
+    else:
+        return _emit(FAIL, 'entity mets.xml files not readable', failed)
+
+def test0442_entity_mets_parsable(entity_metsxml_parsable, entity_metsxml_paths):
+    passed = []
+    failed = []
+    for path in entity_metsxml_paths:
+        if path in entity_metsxml_parsable:
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_metsxml_paths):
+        return _emit(OK, 'entity mets.xml files parsable')
+    else:
+        return _emit(FAIL, 'entity mets.xml files not parsable', failed)
+
+def test0443_entity_mets_valid(entity_metsxml_valid, entity_metsxml_paths):
+    passed = []
+    failed = []
+    for path in entity_metsxml_paths:
+        if path in entity_metsxml_valid:
+            passed.append(path)
+        else:
+            failed.append(path)
+    if len(passed) == len(entity_metsxml_paths):
+        return _emit(OK, 'entity mets.xml files valid')
+    else:
+        return _emit(FAIL, 'entity mets.xml files not valid', failed)
 
 
 def ddr_test_suite(collection_path):
+    x = [
+        'Test suite version: {}'.format(TEST_SUITE_VERSION),
+        'path: {}'.format(collection_path),
+        '---',
+        ]
+    
+    x.append( test00_collection_directory_exists(collection_path) )
     
     changelog_path = os.path.join(collection_path, 'changelog')
     control_path   = os.path.join(collection_path, 'control')
     ead_path       = os.path.join(collection_path, 'ead.xml')
     files_path     = os.path.join(collection_path, 'files')
-    setattr(TestDDRCollection, 'path',                    collection_path)
-    setattr(TestDDRCollection, 'changelog_path',          changelog_path)
-    setattr(TestDDRCollection, 'control_path',            control_path)
-    setattr(TestDDRCollection, 'ead_path',                ead_path)
-    setattr(TestDDRCollection, 'files_path',              files_path)
-
+    
+    x.append( test020_changelog_exists(changelog_path) )
+    x.append( test021_changelog_valid(changelog_path) )
+    x.append( test030_control_exists(control_path) )
+    x.append( test031_control_valid(control_path) )
+    x.append( test010_ead_exists(ead_path)   )
+    x.append( test011_ead_readable(ead_path) )
+    x.append( test013_ead_valid(ead_path) )
+    x.append( test040_entities_dir_exists(collection_path) )
+    
     # Read ead.xml and get list of entities
     entity_eids  = _entity_eids(ead_path)
     entity_paths = _entity_paths(files_path, entity_eids)
-    entity_dirs  = _entity_dirs(entity_paths)
-    setattr(TestDDRCollection, 'entity_eids',             entity_eids)
-    setattr(TestDDRCollection, 'entity_paths',            entity_paths)
-    setattr(TestDDRCollection, 'entity_dirs',             entity_dirs)
-    
+    x.append( test041_entity_dirs_exist(entity_eids, entity_paths) )
+
     # List paths of entity files found in ead.xml.
     # Each entity should have one of these files, so list lengths should match
     # length of entity_paths.
     entity_changelog_paths  = _entity_meta_files(entity_paths, 'changelog')
     entity_control_paths    = _entity_meta_files(entity_paths, 'control')
     entity_metsxml_paths    = _entity_meta_files(entity_paths, 'mets.xml')
-    setattr(TestDDRCollection, 'entity_changelog_paths',  entity_changelog_paths)
-    setattr(TestDDRCollection, 'entity_control_paths',    entity_control_paths)
-    setattr(TestDDRCollection, 'entity_metsxml_paths',    entity_metsxml_paths)
+    x.append( test0420_entity_changelogs_exist(entity_changelog_paths, entity_paths) )
+    x.append( test0430_entity_controls_exist(entity_control_paths, entity_paths) )
+    x.append( test0440_entity_metsxml_exist(entity_metsxml_paths, entity_paths) )
+    x.append( test0421_entity_changelogs_valid(entity_changelog_paths) )
+    x.append( test0431_entity_controls_valid(entity_control_paths) )
     
     # List paths of entity mets.xml files that pass various tests
     entity_metsxml_readable = _entity_xml_filter(entity_paths, 'mets.xml', _read_xml)
     entity_metsxml_parsable = _entity_xml_filter(entity_paths, 'mets.xml', _parse_xml)
     entity_metsxml_valid    = _entity_xml_filter(entity_paths, 'mets.xml', _validate_xml)
-    setattr(TestDDRCollection, 'entity_metsxml_readable', entity_metsxml_readable)
-    setattr(TestDDRCollection, 'entity_metsxml_parsable', entity_metsxml_parsable)
-    setattr(TestDDRCollection, 'entity_metsxml_valid',    entity_metsxml_valid)
+    x.append( test0441_entity_mets_readable(entity_metsxml_readable, entity_metsxml_paths) )
+    x.append( test0442_entity_mets_parsable(entity_metsxml_parsable, entity_metsxml_paths) )
+    x.append( test0443_entity_mets_valid(entity_metsxml_valid, entity_metsxml_paths) )
     
     # List the files for each entity
     entity_files_info       = _entity_files_info(entity_metsxml_readable)
     entity_files_count      = _entity_files_count(entity_files_info)
     entity_files_existing   = _entity_files_existing(entity_files_info)
     entity_files_verified   = _entity_files_verified(entity_files_info)
-    setattr(TestDDRCollection, 'entity_files_info',     entity_files_info)
-    setattr(TestDDRCollection, 'entity_files_count',    entity_files_count)
-    setattr(TestDDRCollection, 'entity_files_existing', entity_files_existing)
-    setattr(TestDDRCollection, 'entity_files_verified', entity_files_verified)
     
-    return unittest.TestLoader().loadTestsFromTestCase(TestDDRCollection)
+    return x
 
 
 if __name__ == '__main__':
     #unittest.main()
     collection_path = './ddr_samples/testcollection'
-    suite = bagit_test_suite(collection_path)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    tap = ddr_test_suite(collection_path)
+    print('\n'.join(tap))
