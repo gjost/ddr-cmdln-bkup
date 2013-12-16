c2708ca9dca97bef50c0ef119c1e3a84cdebd4e9
geoffrey jost <geoffrey.jost@densho.org>
2013-12-16 11:57:31 -0800

    timer for simple profiling
    Initialize the timer, set a bunch of marks, then display deltas
    in logs, slowest steps first.

--- ddr/DDR/__init__.py
+++ ddr/DDR/__init__.py
@@ -20,3 +20,41 @@ def natural_order_string( id ):
     convert = lambda text: int(text) if text.isdigit() else text
     alnum = [ convert(c) for c in re.split('([0-9]+)', id) ]
     return alnum
+
+
+class Timer( object ):
+    """
+    from DDR import Timer
+    t = Timer()
+    t.mark('start')
+    ...YOUR CODE HERE...
+    t.mark('did something')
+    ...MORE CODE...
+    t.mark('did something else')
+    ...MORE CODE...
+    t.display()
+    """
+    steps = []
+    
+    def mark( self, msg ):
+        now = datetime.now()
+        index = len(self.steps)
+        if index:
+            delta = now - self.steps[-1]['datetime']
+        else:
+            delta = timedelta(0)
+        step = {'index':index, 'datetime':now, 'delta':delta, 'msg':msg}
+        self.steps.append(step)
+        logger.debug(msg)
+    
+    def display( self, sorting='descending' ):
+        """Return list of steps arranged slowest first.
+        """
+        from operator import itemgetter
+        steps = sorted(self.steps, key=itemgetter('delta'))
+        if sorting == 'descending':
+            steps.reverse()
+        logger.debug('TIMER RESULTS -- SLOWEST-FASTEST -----------------------------------')
+        for step in steps:
+            logger.debug('{:>10}: {:<14} | {}'.format(step['index'], step['delta'], step['msg']))
+        return steps
