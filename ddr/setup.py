2a244f67c752375595c0c771376c58fb7ede4cb3
geoffrey jost <geoffrey.jost@densho.org>
2014-02-10 17:05:26 -0800

    ddrpubcopy command, for publishing collection binaries
    This command copies the specified types of binaries from a
    collection repo to a specified base directory.  The resulting
    simple file paths will be correctly linked-to from ddr-public.

--- ddr/setup.py
+++ ddr/setup.py
@@ -13,6 +13,6 @@ setup(
     packages = ['DDR'],
     package_dir = {'DDR': 'DDR'},
     package_data = {'DDR': ['*.tpl', 'templates/*',]},
-    scripts = ['bin/ddr', 'bin/ddrfilter',],
+    scripts = ['bin/ddr', 'bin/ddrfilter', 'bin/ddrpubcopy',],
     name = 'ddr'
 )
