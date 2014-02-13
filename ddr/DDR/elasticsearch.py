0ad0f0d6b9208bf21b80ba69c548448d3971b805
geoffrey jost <geoffrey.jost@densho.org>
2014-02-12 20:49:15 -0800

    control index recursion; support new- and old-style ES doc formats
    - Adds recursion flag to index().
    - Fixed settings() URL.
    - Made create_index() and delete_index() into hidden functions.
    - Removed all the *_exists() functions.
    - Renamed get/post/delete functions to more closely match HTTP
      request types.
    - post() now supports both old-style list-of-dicts metadata format
      (ddr-local) in addition to new normal-dict format (ddr-public).
    - Force explicit passing of index arg.
    - ddrindex command

--- ddr/DDR/elasticsearch.py
+++ ddr/DDR/elasticsearch.py
@@ -31,21 +31,31 @@ def _clean_payload(data):
         for field in data:
             _clean_dict(field)
 
-def _metadata_files(dirname):
+def _metadata_files(dirname, recursive=False):
     """Lists absolute paths to .json files in dirname.
     
     Skips/excludes .git directories.
     
-    @param path: Absolute path
+    @param dirname: Absolute path
+    @param recursive: Whether or not to recurse into subdirectories.
     """
     paths = []
-    excludes = ['tmp']
-    for root, dirs, files in os.walk(dirname):
-        if '.git' in dirs:
-            dirs.remove('.git')
-        for f in files:
+    excludes = ['.git', 'tmp']
+    if recursive:
+        for root, dirs, files in os.walk(dirname):
+            # don't go down into .git directory
+            if '.git' in dirs:
+                dirs.remove('.git')
+            for f in files:
+                if f.endswith('.json'):
+                    path = os.path.join(root, f)
+                    exclude = [1 for x in excludes if x in path]
+                    if not exclude:
+                        paths.append(path)
+    else:
+        for f in os.listdir(dirname):
             if f.endswith('.json'):
-                path = os.path.join(root, f)
+                path = os.path.join(dirname, f)
                 exclude = [1 for x in excludes if x in path]
                 if not exclude:
                     paths.append(path)
@@ -59,7 +69,7 @@ def settings(host):
     
     @param host: Hostname and port (HOST:PORT).
     """
-    url = 'http://%s/_status' % (host)
+    url = 'http://%s/_settings' % (host)
     r = requests.get(url)
     return json.loads(r.text)
 
@@ -103,7 +113,7 @@ def _make_mappings():
             mappings['mappings'][model]['properties'][fname] = properties
     return mappings
 
-def create_index(host, index):
+def _create_index(host, index):
     """Create the specified index.
     
     curl -XPOST 'http://localhost:9200/twitter/' -d @mappings.json
@@ -122,7 +132,7 @@ def create_index(host, index):
     logger.debug('%s %s' % (r.status_code, r.text))
     return r.status_code
 
-def delete_index(host, index):
+def _delete_index(host, index):
     """Delete the specified index.
     
     curl -XDELETE 'http://localhost:9200/twitter/'
@@ -137,56 +147,21 @@ def delete_index(host, index):
     logger.debug('%s %s' % (r.status_code, r.text))
     return r.status_code
 
-def index_exists(host, index):
-    """Indicates whether the given ElasticSearch index exists.
-    
-    curl -XHEAD 'http://localhost:9200/ddr/collection'
-    
-    @param host: Hostname and port (HOST:PORT).
-    @param index: Name of the target index.
-    @return True/False
-    """
-    url = 'http://%s/%s' % (host, index)
-    r = requests.head(url)
-    if r.status_code == 200:
-        return True
-    return False
 
-def model_exists(host, index, model):
-    """Indicates whether an ElasticSearch 'type' exists for the given model.
-    
-    curl -XHEAD 'http://localhost:9200/ddr/collection'
-    
-    @param host: Hostname and port (HOST:PORT).
-    @param index: Name of the target index.
-    @param model: Type of object ('collection', 'entity', 'file')
-    @return True/False
-    """
-    url = 'http://%s/%s/%s' % (host, index, model)
-    r = requests.head(url)
-    if r.status_code == 200:
-        return True
-    return False
 
-def document_exists(host, index, model, id):
-    """Indicates whether a given document exists.
-    
-    curl -XHEAD 'http://localhost:9200/ddr/collection/ddr-testing-123'
-    
-    @param host: Hostname and port (HOST:PORT).
-    @param index: Name of the target index.
-    @param model: Type of object ('collection', 'entity', 'file')
-    @param id: object ID
-    @return True/False
-    """
-    url = 'http://%s/%s/%s/%s' % (host, index, model, id)
-    r = requests.head(url)
-    if r.status_code == 200:
-        return True
-    return False
-
-def put_document(path, host, index, model):
+def post(path, host, index, model, newstyle=False):
     """Add a new document to an index or update an existing one.
+    
+    This function can produce ElasticSearch documents in two formats:
+    - old-style list-of-dicts used in the DDR JSON files.
+    - normal dicts used by ddr-public.
+    
+    DDR metadata JSON files are structured as a list of fieldname:value dicts.
+    This is done so that the fields are always in the same order, making it
+    possible to easily see the difference between versions of a file.
+    
+    In ElasticSearch, documents are structured in a normal dict so that faceting
+    works properly.
     
     curl -XPUT 'http://localhost:9200/ddr/collection/ddr-testing-141' -d '{ ... }'
     
@@ -194,9 +169,10 @@ def put_document(path, host, index, model):
     @param host: Hostname and port (HOST:PORT).
     @param index: Name of the target index.
     @param model: Type of object ('collection', 'entity', 'file')
+    @param newstyle: Use new ddr-public ES document format.
     @return 0 if successful, status code if not.
     """
-    logger.debug('put_document(%s, %s, %s, %s)' % (path, index, model, path))
+    logger.debug('post(%s, %s, %s, %s)' % (path, index, model, path))
     if not os.path.exists(path):
         return 1
     headers = {'content-type': 'application/json'}
@@ -204,37 +180,71 @@ def put_document(path, host, index, model):
         filedata = json.loads(f.read())
     _clean_payload(filedata)
     
-    # restructure from list-of-fields dict to a straight dict
-    data = {}
-    for field in filedata:
-        for k,v in field.iteritems():
-            data[k] = v
-    
-    if model in ['collection', 'entity']:
-        if not (data and data.get('id', None)):
-            return 2
-        cid = data['id']
-        url = 'http://%s/%s/%s/%s' % (host, index, model, cid)
+    if newstyle:
+        # restructure from list-of-fields dict to straight dict used by ddr-public
+        data = {}
+        for field in filedata:
+            for k,v in field.iteritems():
+                data[k] = v
         
-    elif model in ['file']:
-        if not (data and data.get('path_rel', None)):
-            return 2
-        filename = None
-        extension = None
-        if data.get('path_rel',None):
-            filename,extension = os.path.splitext(data['path_rel'])
-        basename_orig = data.get('basename_orig', None)
-        label = data.get('label', None)
-        if basename_orig and not label:
-            label = basename_orig
-        elif filename and not label:
-            label = filename
-        data['id'] = filename
-        data['title'] = label
-        url = 'http://%s/%s/%s/%s' % (host, index, model, filename)
+        if model in ['collection', 'entity']:
+            if not (data and data.get('id', None)):
+                return 2
+            cid = data['id']
+            url = 'http://%s/%s/%s/%s' % (host, index, model, cid)
+        elif model in ['file']:
+            if not (data and data.get('path_rel', None)):
+                return 2
+            filename = None
+            extension = None
+            if data.get('path_rel',None):
+                filename,extension = os.path.splitext(data['path_rel'])
+            basename_orig = data.get('basename_orig', None)
+            label = data.get('label', None)
+            if basename_orig and not label:
+                label = basename_orig
+            elif filename and not label:
+                label = filename
+            data['id'] = filename
+            data['title'] = label
+            url = 'http://%s/%s/%s/%s' % (host, index, model, filename)
+        else:
+            url = None
         
     else:
-        url = None
+        # old-style list-of-fields dict used by ddr-local
+        data = filedata
+        
+        if model in ['collection', 'entity']:
+            if not (data and data[1].get('id', None)):
+                return 2
+            cid = None
+            for field in data:
+                if field.get('id',None):
+                    cid = field['id']
+            url = 'http://%s/%s/%s/%s' % (host, index, model, cid)
+        elif model in ['file']:
+            if not (data and data[1].get('path_rel', None)):
+                return 2
+            filename = None
+            basename_orig = None
+            label = None
+            for field in data:
+                if field.get('path_rel',None):
+                    filename,extension = os.path.splitext(field['path_rel'])
+                if field.get('basename_orig', None):
+                    basename_orig = field['basename_orig']
+                if field.get('label', None):
+                    label = field['label']
+            if basename_orig and not label:
+                label = basename_orig
+            elif filename and not label:
+                label = filename
+            data.append({'id': filename})
+            data.append({'title': label})
+            url = 'http://%s/%s/%s/%s' % (host, index, model, filename)
+        else:
+            url = None
     
     if url:
         payload = json.dumps({'d': data})
@@ -245,7 +255,7 @@ def put_document(path, host, index, model):
         return r.status_code
     return 3
 
-def get_document(host, index, model, id):
+def get(host, index, model, id):
     """GET a single document.
     
     GET http://192.168.56.101:9200/ddr/collection/{repo}-{org}-{cid}
@@ -266,7 +276,7 @@ def get_document(host, index, model, id):
         return hits
     return None
 
-def delete_document(host, index, model, id):
+def delete(host, index, model, id):
     """Delete specified document from the index.
     
     curl -XDELETE 'http://localhost:9200/twitter/tweet/1'
@@ -280,17 +290,25 @@ def delete_document(host, index, model, id):
     r = requests.delete(url)
     return r.status_code
 
-def index(dirname, host, index, paths=None):
+
+def index(path, host, index, recursive=False, newstyle=False, paths=None):
     """(Re)index with data from the specified directory.
     
-    @param paths: Absolute paths to directory containing collections.
+    @param path: Absolute path to metadata file or directory containing metadata files.
     @param host: Hostname and port (HOST:PORT).
     @param index: Name of the target index.
+    @param recursive: Whether or not to recurse into subdirectories.
+    @param newstyle: Use new ddr-public ES document format.
+    @param paths: Absolute paths to directory containing collections.
     """
     logger.debug('index(%s, %s)' % (host, index))
     
+    # process a single file if requested
+    if os.path.isfile(path):
+        paths = [path]
+    
     if not paths:
-        paths = _metadata_files(dirname)
+        paths = _metadata_files(path, recursive)
     
     for path in paths:
         model = None
@@ -302,13 +320,13 @@ def index(dirname, host, index, paths=None):
             model = 'file'
         if path and index and model:
             print('adding %s' % path)
-            status = put_document(path, host, index, model)
+            status = post(path, host, index, model, newstyle)
             print(status)
         else:
             logger.error('missing information!: %s' % path)
     logger.debug('INDEXING COMPLETED')
 
-def query(host, index='ddr', model=None, query='', filters={}, sort='', fields='', size=MAX_SIZE):
+def query(host, index, model=None, query='', filters={}, sort='', fields='', size=MAX_SIZE):
     """Run a query, get a list of zero or more hits.
     
     curl -XGET 'http://localhost:9200/twitter/tweet/_search?q=user:kimchy&pretty=true'
