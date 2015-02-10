91f20d9ce743bc04459b72af8c5a4accaff36b2c
geoffrey jost <geoffrey.jost@densho.org>
2015-02-10 10:18:25 -0800

    reformat setup.py

--- ddr/setup.py
+++ ddr/setup.py
@@ -9,10 +9,29 @@ setup(
     download_url = 'https://github.com/densho/ddr-cmdln.git',
     author_email = 'geoffrey.jost@densho.org',
     version = '0.9-beta',
-    install_requires = ['nose'],
-    packages = ['DDR', 'DDR.converters'],
-    package_dir = {'DDR': 'DDR'},
-    package_data = {'DDR': ['*.tpl', 'models/*', 'templates/*',]},
-    scripts = ['bin/ddr', 'bin/ddr-checkencoding', 'bin/ddrfilter', 'bin/ddrindex', 'bin/ddrmassupdate', 'bin/ddrpubcopy', 'bin/ddrdensho255fix'],
+    install_requires = [
+        'nose'
+    ],
+    packages = [
+        'DDR',
+        'DDR.converters'
+    ],
+    package_dir = {
+        'DDR': 'DDR'
+    },
+    package_data = {'DDR': [
+        '*.tpl',
+        'models/*',
+        'templates/*',
+    ]},
+    scripts = [
+        'bin/ddr',
+        'bin/ddr-checkencoding',
+        'bin/ddrfilter',
+        'bin/ddrindex',
+        'bin/ddrmassupdate',
+        'bin/ddrpubcopy',
+        'bin/ddrdensho255fix',
+    ],
     name = 'ddr'
 )
