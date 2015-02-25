2c30aa17fb630223d0019494a44a559a62fcf708
geoffrey jost <geoffrey.jost@densho.org>
2015-02-25 12:54:43 -0800

    setup.py loads version and long description automatically

--- ddr/setup.py
+++ ddr/setup.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 
+import codecs
 from distutils.core import setup
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^VERSION = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = read('README.rst')
 
 setup(
     description = 'ddr-cmdln',
     author = 'Geoffrey Jost',
+    version = find_version('DDR', '__init__.py'),
     url = 'https://github.com/densho/ddr-cmdln/',
     download_url = 'https://github.com/densho/ddr-cmdln.git',
     author_email = 'geoffrey.jost@densho.org',
-    version = '0.9-beta',
+    long_description = long_description,
     install_requires = [
         'nose'
     ],
