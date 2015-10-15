8bea62ab38c9d48137ed916627c3bebe7c08bb02
geoffrey jost <geoffrey.jost@densho.org>
2015-10-15 13:43:39 -0700

    unit tests for DDR.fileio

--- ddr/DDR/tests_fileio.py
+++ ddr/DDR/tests_fileio.py
@@ -3,14 +3,22 @@ import os
 import fileio
 
 
-# TODO read_text
+TEXT = '{"a": 1, "b": 2}'
+
+def test_read_text():
+    path = '/tmp/test_DDR.fileio.read_text.json'
+    with open(path, 'w') as f:
+        f.write(TEXT)
+    data = fileio.read_text(path)
+    assert data == TEXT
+    # clean up
+    os.remove(path)
 
 def test_write_text():
-    data = {'a':1, 'b':2}
-    path = '/tmp/ddrlocal.models.write_text.json'
-    fileio.write_text(data, path)
+    path = '/tmp/test_DDR.fileio.write_text.json'
+    fileio.write_text(TEXT, path)
     with open(path, 'r') as f:
-        written = f.readlines()
-    assert written == ['{\n', '    "a": 1,\n', '    "b": 2\n', '}']
+        written = f.read()
+    assert written == TEXT
     # clean up
     os.remove(path)
