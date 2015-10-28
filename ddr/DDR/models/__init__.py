9c075174fd606aad93b34edc52f3c222988aa77d
geoffrey jost <geoffrey.jost@densho.org>
2015-10-28 10:10:34 -0700

    ideas for refactoring object field storage

--- ddr/DDR/models/__init__.py
+++ ddr/DDR/models/__init__.py
@@ -2,6 +2,29 @@
 NOTE: Much of the code in this module used to be in ddr-local
 (ddr-local/ddrlocal/ddrlocal/models/__init__.py).  Please refer to that project
 for history prior to Feb 2015.
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+TODO refactor: keep metadata from json_data
+
+TODO refactor: load json_text into an OrderedDict
+
+TODO refactor: put json_data in a object.source dict like ES does.
+
+This way we don't have to worry about field names conflicting with
+class methods (e.g. Entity.parent).
+
+ACCESS that dict (actually OrderedDict) via object.source() method.
+Lazy loading: don't load unless something needs to access the data
+
+IIRC the only time we need those fields is when we display the object
+to the user.
+
+Also we won't have to reload the flippin' .json file multiple times
+for things like cmp_model_definition_fields.
+
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 """
 
 from datetime import datetime
