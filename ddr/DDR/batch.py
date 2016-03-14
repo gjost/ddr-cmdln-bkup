66d82d107f4e1f817669d58cb1a9ed9826e6985a
geoffrey jost <geoffrey.jost@densho.org>
2016-03-14 13:59:21 -0700

    ddr-export,import: mv all export,import code into batch.py

--- ddr/DDR/batch.py
+++ ddr/DDR/batch.py
@@ -1,4 +1,6 @@
 """
+AKA cumberbatch.py
+
 Check to see if CSV file is internally valid
 See which EIDs would be added
 Update existing records
@@ -6,6 +8,7 @@ Import new records
 Register newly added EIDs
 """
 
+import codecs
 from datetime import datetime
 import json
 import logging
@@ -26,6 +29,68 @@ from DDR import modules
 from DDR import util
 
 COLLECTION_FILES_PREFIX = 'files'
+
+
+class Exporter():
+    
+    @staticmethod
+    def _make_tmpdir(tmpdir):
+        """Make tmp dir if doesn't exist.
+        
+        @param tmpdir: Absolute path to dir
+        """
+        if not os.path.exists(tmpdir):
+            os.makedirs(tmpdir)
+
+    @staticmethod
+    def export(json_paths, model, csv_path, required_only=False):
+        """Write the specified objects' data to CSV.
+        
+        IMPORTANT: All objects in json_paths must have the same set of fields!
+        
+        TODO let user specify which fields to write
+        TODO confirm that each identifier's class matches object_class
+        
+        @param json_paths: list of .json files
+        @param model: str
+        @param csv_path: Absolute path to CSV data file.
+        @param required_only: boolean Only required fields.
+        """
+        object_class = identifier.class_for_name(
+            identifier.MODEL_CLASSES[model]['module'],
+            identifier.MODEL_CLASSES[model]['class']
+        )
+        module = modules.Module(identifier.module_for_name(
+            identifier.MODEL_REPO_MODELS[model]['module']
+        ))
+        
+        if hasattr(object_class, 'xmp') and not hasattr(object_class, 'mets'):
+            # File or subclass
+            json_paths = models.sort_file_paths(json_paths)
+        else:
+            # Entity or subclass
+            json_paths = util.natural_sort(json_paths)
+        json_paths_len = len(json_paths)
+        
+        Exporter._make_tmpdir(os.path.dirname(csv_path))
+        
+        headers = module.csv_export_fields(required_only)
+        # make sure we export 'id' if it's not in model FIELDS (ahem, files)
+        if 'id' not in headers:
+            headers.insert(0, 'id')
+        
+        with codecs.open(csv_path, 'wb', 'utf-8') as csvfile:
+            writer = fileio.csv_writer(csvfile)
+            # headers in first line
+            writer.writerow(headers)
+            for n,json_path in enumerate(json_paths):
+                i = identifier.Identifier(json_path)
+                logging.info('%s/%s - %s' % (n+1, json_paths_len, i.id))
+                obj = object_class.from_identifier(i)
+                if obj:
+                    writer.writerow(obj.dump_csv(headers=headers))
+        
+        return csv_path
 
 
 class ModifiedFilesError(Exception):
